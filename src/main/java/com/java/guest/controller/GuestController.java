228fefe886ab1fecc8d888456cb1725933ce121c
egeoda <egeoda@protonmail.com>
2019-12-05 16:44:10 +0900

    qaaa

--- src/main/java/com/java/guest/controller/GuestController.java
+++ src/main/java/com/java/guest/controller/GuestController.java
@@ -62,6 +62,7 @@ public class GuestController extends MultiActionController {
 		modelAndView.addObject("guestDto", guestDto);
 		guestService.guestWriteOk(modelAndView);
 		System.out.println("gwo OK");
+		System.out.println("AAAA");
 		return modelAndView;
 	}
 
